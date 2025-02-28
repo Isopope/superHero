e1d0261c41ecc230350537e3b63f5f628cef2e0a
Isopope <nkoubah@gmail.com>
2025-02-28 21:12:04 +0100

    definition of getAllSuperHeroes and searchSuperHeroes methods in SuperheroService

--- src/app/services/superhero.service.ts
+++ src/app/services/superhero.service.ts
@@ -1,9 +1,32 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Superhero } from '../models/superhero';
+import { map, Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SuperheroService {
+  private dataSource="https://akabab.github.io/superhero-api/api/all.json";
 
-  constructor() { }
+
+  constructor(private readonly http: HttpClient) { }
+
+  getAllSuperheroes(): Observable<Superhero[]> {
+    return this.http.get<Superhero[]>(this.dataSource);
+  }
+
+  searchSuperheroes(name: string): Observable<Superhero[]> {
+    return this.getAllSuperheroes().pipe(
+      map(superheroes => {
+        const filteredHeroes: Superhero[] = [];
+        superheroes.forEach(hero => {
+          if (hero.name.toLowerCase().includes(name.toLowerCase())) {
+            filteredHeroes.push(hero);
+          }
+        });
+        return filteredHeroes;
+      })
+    );
+  }
 }
